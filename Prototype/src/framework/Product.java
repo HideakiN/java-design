de8c6934add688780cb79ba96b05f486f0adf3d0
Hideaki Nakaishi <lovely_hide_chan@live.jp>
2021-09-18 01:34:01 +0900

    Add Prototype pattern

---
+++ Prototype/src/framework/Product.java
@@ -0,0 +1,17 @@
+package framework;
+
+public abstract class Product implements Cloneable {
+
+	public abstract void use(String s);
+	public Product createClone() {
+		Product p = null;
+		try {
+			p = (Product)this.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+		return p;
+	}
+
+}
